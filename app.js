fa88abf01fb7204039ba1d7933682eb3004646c6
sajith sajeev <sajithsajeevr@outlook.com>
2018-08-02 13:15:59 +0530

    for each done and ddusted

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-alert(haiiii);
+const companies= [
+    {name: "Company One", category: "Finance", start: 1981, end: 2004},
+    {name: "Company Two", category: "Retail", start: 1992, end: 2008},
+    {name: "Company Three", category: "Auto", start: 1999, end: 2007},
+    {name: "Company Four", category: "Retail", start: 1989, end: 2010},
+    {name: "Company Five", category: "Technology", start: 2009, end: 2014},
+    {name: "Company Six", category: "Finance", start: 1987, end: 2010},
+    {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
+    {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
+    {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
+  ];
+  
+  const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+
+ /*  companies.forEach(function(Company,index,companies){
+console.log(Company.category);
+  });
+
+
+  companies.forEach(company => {
+  console.log(company.category);    
+  }); */
+
+  //filter vs for
+const canDrink=[];
+  for(let i=0;i<ages.length;i++){
+      if(ages[i]<18){
+console.log(ages[i]);
+canDrink.push(ages[i]);
+
+
+  }}
+  console.log(canDrink);
+
+
+  
